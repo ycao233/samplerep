c6298f4600b4e2c8a0753c87c79195b2d3998ac7
Prawal Agarwal <praga@microsoft.com>
2016-05-25 14:53:26 +0530

    Adding project

---
+++ SmallProject/SmallProject/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmallProject
+{
+    public static class Class1
+    {
+        public static int AddMethod(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int SubtractMethod(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int MultiplyMethod(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
---
+++ SmallProject/SmallProjectTests/UnitTest1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SmallProject;
+using SupportSampleProject;
+
+namespace SmallProjectTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var appSettings = ConfigurationManager.AppSettings;
+            Assert.AreEqual(appSettings["Name"], "Prawal");
+            Assert.AreEqual(4, Class1.AddMethod(2, 2));
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            SupportClass cls = new SupportClass();
+
+            Assert.AreEqual(cls.addTwo(2,2), Class1.AddMethod(2, 2));
+        }
+
+        [TestMethod]
+        public void TestMethodWithChineseKeyboard开月在()
+        {
+            Assert.AreEqual(4, Class1.MultiplyMethod(2, 2));
+        }
+
+        [TestMethod]
+        public void TestMethodWithChineseKeyboard2到后也()
+        {
+            Assert.AreEqual(4, Class1.MultiplyMethod(2, 2));
+        }
+
+        [TestMethod]
+        public void TestMethodWithChineseCopyDoubleByte狜隣郎()
+        {
+            Assert.AreEqual(4, Class1.MultiplyMethod(2, 2));
+        }
+
+        [TestMethod]
+        public void TestMethodWithChineseCopyFourByte䵯䵰䶴()
+        {
+            Assert.AreEqual(4, Class1.MultiplyMethod(2, 2));
+        }
+
+        [TestMethod]
+        public void TestMethodWithTibetanCopyཉཪཱྋཇཉ()
+        {
+            Assert.AreEqual(4, Class1.MultiplyMethod(2, 2));
+        }
+
+        [TestMethod]
+        public void TestMethodWithTibetanད༣མ༦ཡ༢()
+        {
+            Assert.AreEqual(4, Class1.MultiplyMethod(2, 2));
+        }
+
+        [TestMethod]
+        public void TestMethodWithJPNKeyboardはぬぎ()
+        {
+            Assert.AreEqual(4, Class1.MultiplyMethod(2, 2));
+        }
+
+        [TestMethod]
+        public void TestMethodWithArabicKeyboardلغبسعلا()
+        {
+            Assert.AreEqual(4, Class1.MultiplyMethod(2, 2));
+        }
+
+        [TestMethod]
+        public void TestMethodWithGermanKeyboardÜßäö()
+        {
+            Assert.AreEqual(4, Class1.MultiplyMethod(2, 2));
+        }
+
+        [TestMethod]
+        public void TestMethodWithChineseTaiwanKeyboard好你直()
+        {
+            Assert.AreEqual(4, Class1.MultiplyMethod(2, 2));
+        }
+
+
+    }
+}
