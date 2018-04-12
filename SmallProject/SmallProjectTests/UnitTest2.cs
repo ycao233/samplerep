b4d3599b539dc55476dd570db4da86eac1c8e70e
Prawal Agarwal <praga@microsoft.com>
2018-04-12 16:00:29 +0530

    Update UnitTest2.cs

--- SmallProject/SmallProjectTests/UnitTest2.cs
+++ SmallProject/SmallProjectTests/UnitTest2.cs
@@ -1,1 +1,28 @@
 
+using System;
+using System.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SmallProject;
+using SupportSampleProject;
+
+namespace SmallProjectTests
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void TestMethodNew1()
+        {
+            
+        }
+
+        [TestMethod]
+        public void TestMethodNew2()
+        {
+            SupportClass cls = new SupportClass();
+
+            Assert.AreEqual(cls.addTwo(2,2), Class1.AddMethod(2, 2));
+        }
+
+    }
+}
